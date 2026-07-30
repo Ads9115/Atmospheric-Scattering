0cf5754b164437900227169002e603260c2669a9
CrumblingBread <adarshsen9115@gmail.com>
2026-07-30 14:55:26 +0530

    Added : from Triangle to a quad

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,15 @@
 
 float vertices[] = {
 	// Positions          // Colors
-	 0.0f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Top vertex (Red)
-	-0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom-left vertex (Green)
-	 0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Bottom-right vertex (Blue)
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // 0: Top-Right (Red)
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // 1: Bottom-Right (Green)
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  // 2: Bottom-Left (Blue)
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f   // 3: Top-Left (Yellow)
+};
+
+unsigned int indices[] = {
+	0, 1, 3,  // First Triangle (Top-Right half)
+	1, 2, 3   // Second Triangle (Bottom-Left half)
 };
 
 
@@ -41,14 +47,18 @@ int main() {
 	std::cout << std::filesystem::current_path() << '\n';
 	Shader shader("shaders/vertexShader.vert", "shaders/fragmentShader.frag");
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Position
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -73,7 +83,7 @@ int main() {
 
 		shader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		glfwSwapBuffers(window);
@@ -82,6 +92,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
